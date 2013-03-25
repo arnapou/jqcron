94353aed5d2245d5f9a4dff801d6f3c5f7506618
Arnaud Buathier <arnaud@arnapou.net>
2013-03-25 20:55:33 +0100

    license info

--- src/Cron.php
+++ src/Cron.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Arnapou jqCron package.
+ *
+ * (c) Arnaud Buathier <arnaud@arnapou.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 class Cron {
 	const TYPE_UNDEFINED = '';
 	const TYPE_MINUTE = 'minute';
