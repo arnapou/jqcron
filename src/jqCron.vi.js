1dc2eedede72704f099464d0620793c9faebc6f7
huseyinturkmenoglu <HuseyinTurkmenoglu@users.noreply.github.com>
2019-09-15 19:54:35 +0300

    adds Vietnam Language
    Hi, this file support Vietnam language. We set up for our product. We want to share with you.

---
+++ src/jqCron.vi.js
@@ -0,0 +1,35 @@
+/*
+ * This file is part of the Arnapou jqCron package.
+ *
+ * (c) Arnaud Buathier <arnaud@arnapou.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+jqCronDefaultSettings.texts.vi = {
+	empty_minutes: 'mỗi',
+	empty_time_hours: 'mỗi tiếng',
+	empty_time_minutes: 'mỗi phút',
+	empty_day_of_week: 'mỗi ngày trong tuần',
+	empty_day_of_month: 'mỗi ngày trong tháng',
+	empty_month: 'mỗi tháng',
+	name_minute: 'phút',
+	name_hour: 'giờ',
+	name_day: 'ngày',
+	name_week: 'tuần',
+	name_month: 'tháng',
+	name_year: 'năm',
+	text_period: 'Mỗi <b />',
+	text_mins: ' tại <b /> (vài) phút trước giờ',
+	text_time: ' tại <b />:<b />',
+	text_dow: ' trên <b />',
+	text_month: ' của <b />',
+	text_dom: ' trên <b />',
+	error1: 'Thẻ %s là không được hỗ trợ !',
+	error2: 'Xấu số của các yếu tố',
+	error3: 'Các jquery_element nên được đặt vào jqCron thiết lập.',
+	error4: 'Biểu hiện không được công nhận',
+	weekdays: ['thứ hai', 'thứ ba', 'thứ tư', 'thứ năm', 'thứ sáu', 'ngày thứ bảy', 'chủ nhật'],
+	months: ['tháng giêng', 'tháng hai', 'tháng ba', 'tháng tư', 'có thể', 'tháng sáu', 'tháng bảy', 'tháng tám', 'tháng chín', 'tháng mười', 'tháng mười một', 'tháng mười hai']
+};
