44011c8122a14e38797e73aff418376786b795bd
Arnaud Buathier <arnaud@arnapou.net>
2020-07-06 20:16:26 +0200

    feat: add italian translation Issue #51
    thanks to @antman3351 (Antonio Norman)

---
+++ src/jqCron.it.js
@@ -0,0 +1,37 @@
+/*
+ * This file is part of the Arnapou jqCron package.
+ *
+ * (c) Arnaud Buathier <arnaud@arnapou.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+jqCronDefaultSettings.texts.fr = {
+	empty: 'ogni',
+	empty_minutes: 'ogni',
+	empty_time_hours: 'ogni ora',
+	empty_time_minutes: 'ogni minuto',
+	empty_day_of_week: 'ogni giorno della settimana',
+	empty_day_of_month: 'ogni giorno del mese',
+	empty_month: 'ogni mese',
+	name_minute: 'minuto',
+	name_hour: 'ora',
+	name_day: 'giorno',
+	name_week: 'settimana',
+	name_month: 'mese',
+	name_year: 'anno',
+	text_period: 'Ogni <b />',
+	text_mins: ' a <b /> minuto(i) dopo l\'ora',
+	text_time: ' alle <b />:<b />',
+	text_dow: ' al <b />',
+	text_month: ' di <b />',
+	text_dom: ' al <b />',
+	error1: 'Il tag %s non è supportato !',
+	error2: 'Numero di elementi non corretto',
+	error3: 'Il "jquery_element" deve essere impostato nelle impostazioni di jqCron',
+	error4: 'Espressione sconosciuto',
+	weekdays: ['lunedì', 'martedì', 'mercoledì', 'giovedì', 'venerdì', 'sabato', 'domenica'],
+	months: ['gennaio', 'febbraio', 'marzo', 'aprile', 'maggio', 'giugno', 'luglio', 'agosto', 'settembre', 'ottobre', 'novembre', 'dicembre']
+};
+
